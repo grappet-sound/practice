93c24b8e9e7bad6a5926b4b558ce5b14d8bc6255
N07H1N9 <n07h1n9@192.168.1.3>
2024-04-21 18:04:18 +0700

    April 21

--- src/screens/Main.js
+++ src/screens/Main.js
@@ -1,12 +1,38 @@
 import { Link } from "react-router-dom";
+import "../style/Main.css";
+import { HashLink } from "react-router-hash-link";
 function Main() {
     return(
-        <div>
-            <Link to='/random'>
-                <button>
-                    Random Game
-                </button>
-            </Link>
+        <div className="body">
+            <div className="topbar">
+                <HashLink smooth to='/#main'>
+                    <div>Main</div>
+                </HashLink>
+                <HashLink smooth to='/#game'>
+                    <div>Games</div>
+                </HashLink>
+            </div>
+            <div className="screen" id="main">
+                Main
+            </div>
+            <div className="screen" id="game">
+                <h3>Games</h3>
+                <div className="gameContainer">
+                    <Link to='/random'>
+                        <button>
+                            Random Game
+                        </button>
+                    </Link>
+                    <Link to='/tictactoe'>
+                        <button>
+                            TicTacToe Game
+                        </button>
+                    </Link>
+                </div>
+                
+            </div>
+            
+            
         </div>
     );
 }
